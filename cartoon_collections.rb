81d972e6c65dcbbe07ae38ae499e82f7ffd50b64
Nataliya Rudenko <n.rudenko@gmail.com>
2019-01-30 01:55:06 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,7 +5,8 @@ def roll_call_dwarves(dwarvers)
 end
 
 def summon_captain_planet(veggies)
-  veggies.capitalize +"!"
+  veggies.split
+  veggies.capitalize << "!"
 end
 
 def long_planeteer_calls# code an argument here
