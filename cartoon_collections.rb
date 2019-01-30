8181aa3825fded298bd81bfe5e018dd43fa9f852
Nataliya Rudenko <n.rudenko@gmail.com>
2019-01-30 02:56:46 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -18,8 +18,8 @@ def long_planeteer_calls(calls_long)
 
 def find_the_cheese(cheddar_cheese)
   return cheddar_cheese[1]
+end
   cheese_types = ["cheddar", "gouda", "camembert"]
   cheddar_cheese.find do |cheese|
   cheese.include?("ham")
-  end
 end
