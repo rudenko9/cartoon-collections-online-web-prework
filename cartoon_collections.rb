ca156cbdae5df4399a4d998518e102a9e8fce1ca
Nataliya Rudenko <n.rudenko@gmail.com>
2019-01-30 02:57:40 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -20,6 +20,6 @@ def find_the_cheese(cheddar_cheese)
   puts cheddar_cheese[1]
   cheese_types = ["cheddar", "gouda", "camembert"]
   cheddar_cheese.find do |cheese|
-  cheese.include?("ham")
+  cheese.include?("cheese")
 end
 end
