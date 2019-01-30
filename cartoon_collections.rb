3ac3483e8effa2c7d22fcfd2ac8af5afbad991c9
Nataliya Rudenko <n.rudenko@gmail.com>
2019-01-30 02:01:30 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,8 +10,9 @@ def summon_captain_planet(veggies)
   end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(calls_long)
+  
+
 end
 
 def find_the_cheese# code an argument here
